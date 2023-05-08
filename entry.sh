3bdccc421a8d0859678dc97c286b638b39bcabb1
Shravan Aras <123.shravan@gmail.com>
2023-05-08 15:30:56 -0700

    Bug fixes.

--- entry.sh
+++ entry.sh
@@ -3,15 +3,13 @@ echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": 
 
 # Create a folder to find the external volume
 BASEDIR=/data-store/iplant/home/$IPLANT_USER/
-mkdir $BASEDIR/superset-sqlite
+mkdir -p $BASEDIR/superset-sqlite
 
 export SUPERSET_HOME=$BASEDIR/superset-sqlite/
 
 superset db upgrade && \
-    superset fab create-admin --username "admin" --firstname "Superset" \
+    superset fab create-admin --username $SUPERSET_ADMIN_USER --firstname "Superset" \
                                 --lastname "admin" --email "admin@foo.org" \
-                                --password "admin" && \
-superset init
+                                --password $SUPERSET_ADMIN_PASS && superset init
 
 exec superset run -h 0.0.0.0 -p 9088 --with-threads
-
