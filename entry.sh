87a7be728ae8176c7213c3dd3bb0e38d0a0ad4da
Shravan Aras <123.shravan@gmail.com>
2023-05-08 16:20:04 -0700

    Adding cyverse path corrections

--- entry.sh
+++ entry.sh
@@ -2,14 +2,14 @@
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
 # Create a folder to find the external volume
-BASEDIR=/data-store/iplant/home/$IPLANT_USER/
-mkdir -p $BASEDIR/superset-sqlite
+mkdir -p /data-store/iplant/home/$IPLANT_USER/superset-sqlite
 
-export SUPERSET_HOME=$BASEDIR/superset-sqlite/
+export SUPERSET_HOME=/data-store/iplant/home/$IPLANT_USER/superset-sqlite/
 
-superset db upgrade && \
-    superset fab create-admin --username $SUPERSET_ADMIN_USER --firstname "Superset" \
+superset db upgrade 
+superset fab create-admin --username $SUPERSET_ADMIN_USER --firstname "Superset" \
                                 --lastname "admin" --email "admin@foo.org" \
-                                --password $SUPERSET_ADMIN_PASS && superset init
+                                --password $SUPERSET_ADMIN_PASS
+superset init
 
 exec superset run -h 0.0.0.0 -p 9088 --with-threads
