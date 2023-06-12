2b8dd2db735e12842b9e06db2bbf0e7afdff7280
Shravan Aras <123.shravan@gmail.com>
2023-06-12 11:33:06 -0700

    Bug fix for cronjob
    1. IPLANT_USER auto-finding has been fixed.
    2. metadata file is also written to the analysis output folder.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Get the name of the current iplant user so we can form the path
-IPLANT_USER=`ls -1 | grep --invert-match "shared"`
-cp /var/superset/superset.db /data-store/iplant/home/$IPLANT_USER/superset-sqlite/ 
+# We cannot use the env variables here, as they are not set in the context of the cron job being called.
+IPLANT_USER=`ls -1 /data-store/iplant/home/ | grep --invert-match "shared"`
+cp /var/superset/superset.db /data-store/iplant/home/"$IPLANT_USER"/superset-sqlite/
+# Also copy it to the output folder
+cp /var/superset/superset.db /data-store/output/
+# A test copy to the working directory (used for local testing)
 cp /var/superset/superset.db /var/superset-home/
-#chown superset:superset /data-store/iplant/home/$IPLANT_USER/superset-sqlite/superset.db
