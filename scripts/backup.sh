50422da28b62e6ac0962724014ce8bab54892645
Shravan Aras <123.shravan@gmail.com>
2023-06-12 10:51:47 -0700

    Added a way to test out cron job.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,5 +3,5 @@
 # Get the name of the current iplant user so we can form the path
 IPLANT_USER=`ls -1 | grep --invert-match "shared"`
 cp /var/superset/superset.db /data-store/iplant/home/$IPLANT_USER/superset-sqlite/ 
-#cp /var/superset/superset.db /home/superset/
+cp /var/superset/superset.db /var/superset-home/
 #chown superset:superset /data-store/iplant/home/$IPLANT_USER/superset-sqlite/superset.db
